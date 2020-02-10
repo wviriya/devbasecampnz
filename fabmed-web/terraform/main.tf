3d0bc8eb74f3f46b8637b5ee86d3222930aab2ce
Victor Viriya-ampanond <wviriya@microsoft.com>
2020-02-10 13:52:38 +1300

    remove code

--- fabmed-web/terraform/main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "terraform_rg"
-    storage_account_name = "wviriyatfstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
-    access_key           = "__terraform-backend-key__"
-  }
-}
--- myshuttle/README.md
+++
@@ -1,4 +0,0 @@
-# MyShuttle2
--------------
-
-This is a sample Java/JEE based application that was used in the Connect 2016 demo. 
